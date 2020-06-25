63ff5527387786ddaf69f4e1e5b14f03a0d82b00
Nadilla Rahma <rfkokt105@MacBook-Air-Rfkokt105.local>
2020-06-26 01:01:30 +0700

    update mac

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
-const menusamping = document.querySelector('.menusamping');
-const penutup_samping = document.querySelector('.penutup_samping');
-const bars = document.getElementById('bars-me');
+// Side Bar
+const menusamping = document.querySelector(".menusamping");
+const penutup_samping = document.querySelector(".penutup_samping");
+const bars = document.getElementById("bars-me");
 
 bars.addEventListener("click", () => {
-    menusamping.classList.add("aktif");
-    penutup_samping.classList.add("tampil");
+  menusamping.classList.add("aktif");
+  penutup_samping.classList.add("tampil");
 });
 
 penutup_samping.addEventListener("click", () => {
-    menusamping.classList.remove("aktif");
-    penutup_samping.classList.remove("tampil");
+  menusamping.classList.remove("aktif");
+  penutup_samping.classList.remove("tampil");
 });
+
+// Side Bar End
+
+// Navbar
+
+const navbar = document.getElementById("navbar");
+
+document.addEventListener("scroll", () => {
+  if (window.top.scrollY > 199) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+// Navbar End
